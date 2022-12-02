4c200db8000b8035a6d439bdc57aa904eab60f8b
Konstantin Borisov <68741206+Anexxxus@users.noreply.github.com>
2022-12-02 21:33:28 +0300

    Add files via upload

---
+++ кр1.pas
@@ -0,0 +1,14 @@
+﻿begin
+var t: boolean;
+var a: array[1..7] of integer;
+  Print('Введите число массива');
+  t := false;
+  for var i := 1 to 7 do
+  read(a[i]);
+  for var i := 1 to 7 - 2 do
+    if (a[i] + a[i + 1]) <> (a[i + 2]) then
+      t := false
+    else t := true;
+  Print(t)
+end.
+
---
+++ кр2.pas
@@ -0,0 +1,26 @@
+﻿begin
+  var str, pstr: string;
+  readln(str);
+  readln(pstr);
+  var n := 0;
+  var i := 1;
+  while i <= length(str) do
+    if str[i] = pstr[1] then
+    begin
+      var j := 1;
+      while (j <= length(pstr)) and (str[i + j - 1] = pstr[j]) do j := j + 1;
+      if j > length(pstr) then  
+        n := n + 1;
+      if n mod 2 = 0 then
+      begin
+        for j := 1 to length(pstr) do
+        begin
+          for var k := i to length(str) - 1 do str[k] := str[k + 1];
+          setlength(str, length(str) - 1);
+        end
+      end
+      else i := i + 1;
+    end
+    else i := i + 1;
+  print(str);
+end.
